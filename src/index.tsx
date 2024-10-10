73416257cd2711d66ccd4aab6423d378e139aa0a
Edward <edwardduong71311@gmail.com>
2024-10-10 16:46:36 -0500

    fix(CS545-6): rename App to Lab 6

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import App_Lab6 from './App_Lab6';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+    <App_Lab6 />
 );
 
